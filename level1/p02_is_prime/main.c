82bc89fe3ea86463a135b430d32fc9bf4d902d15
Terkrr <81885228@qq.com>
2026-09-23 21:09:02 +0800

    Add primality test using Miller-Rabin algorithm
    Implement Miller-Rabin primality test for large numbers.

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,38 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+//Miller-Rabin 复杂度7logn 支持1e18级别 
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll a[7]={2,325,9375,28178,450775,9780504,1795265022};
+ll fpow(ll a,ll b,ll mod){
+	ll s=1;
+	while(b){
+		if(b&1) s=(__int128)s*a%mod;
+		a=(__int128)a*a%mod;
+		b>>=1;
+	}
+	return s;
+}
+int check(ll x){
+	if(x==1) return 0;
+	if(x==2) return 1;
+	if(x%2==0) return 0;
+	for(int i=0;i<7;i++){
+    	ll d=x-1;
+   		while(d%2==0) d>>=1;
+    	ll t=fpow(a[i],d,x);
+    	if(!t) continue;
+    	while((d!=x-1)&&(t!=1)&&(t!=x-1)){
+     		t=(__int128)t*t%x;
+     		d=d<<1;
+    	}
+    	if(!((t==x-1)||(d%2))) return 0;
+	}
+	return 1;
+}
+int main(){
+	ll x;
+	cin>>x;
+	if(check(x)) cout<<x<<" is a prime";
+	else cout<<x<<" isn't a prime";
+	return 0;
 }
